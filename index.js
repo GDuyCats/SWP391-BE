15169773632da13c20e264e17538ac8345e49cb3
GDuyCats <luongduycat@gmail.com>
2025-10-03 17:21:00 +0700

    hard fix

--- index.js
+++ index.js
@@ -45,8 +45,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // ⛳ ĐÃ SỬA: preflight
-app.options("*", cors(corsOptions));
-
+app.options(/.*/, cors(corsOptions)); 
 // Routes
 app.use(admin_routes);
 app.use(auth_routes);
