23de0b89127808181840b81f71f1107614fbcb42
GDuyCats <luongduycat@gmail.com>
2025-10-06 17:14:17 +0700

    fix

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,7 +2,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const FROM = process.env.RESEND_FROM || "2NDEV <onboarding@resend.dev>";
+const FROM = process.env.RESEND_FROM || "luongduycat@gmail.com";
 
 export default class Mail {
   constructor() {
